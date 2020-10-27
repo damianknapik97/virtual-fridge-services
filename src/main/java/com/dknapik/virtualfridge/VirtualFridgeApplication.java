701cbd0b3d35d1644670ab1ab681aa89f0d9071f
dknapik <dknapik@kamsoft.pl>
2020-10-27 10:53:18 +0100

    Initialize spring boot project

---
+++ src/main/java/com/dknapik/virtualfridge/VirtualFridgeApplication.java
@@ -0,0 +1,13 @@
+package com.dknapik.virtualfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualFridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualFridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dknapik/virtualfridge/VirtualFridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dknapik.virtualfridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualFridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
